90ac597277c96d5e7291d86fe2e02205d5998616
Roda <rodaissa908@gmail.com>
2024-10-06 19:09:26 -0400

    Initial commit - added lab5 exercise

---
+++ week05_lab_execrcise05/index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json());
+
+/*
+- Create new html file named home.html 
+- add <h1> tag with the message "Welcome to ExpressJs Tutorial"
+- Return home.html page to client
+*/
+router.get('/home', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+});
+
+/*
+- Return all details from user.json file to client as JSON format
+*/
+router.get('/profile', (req, res) => {
+  fs.readFile('user.json', 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to load user data' });
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+/*
+- Modify /login router to accept username and password as JSON body parameter
+- Read data from user.json file
+- If username and password are valid then send response as:
+    {
+        status: true,
+        message: "User Is valid"
+    }
+- If username is invalid then send response as:
+    {
+        status: false,
+        message: "User Name is invalid"
+    }
+- If password is invalid then send response as:
+    {
+        status: false,
+        message: "Password is invalid"
+    }
+*/
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  fs.readFile('user.json', 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to load user data' });
+    }
+
+    const user = JSON.parse(data);
+
+    if (user.username !== username) {
+      return res.json({ status: false, message: "User Name is invalid" });
+    }
+    if (user.password !== password) {
+      return res.json({ status: false, message: "Password is invalid" });
+    }
+    res.json({ status: true, message: "User Is valid" });
+  });
+});
+
+/*
+- Modify /logout route to accept username as parameter and display message
+    in HTML format like <b>${username} successfully logout.<b>
+*/
+router.get('/logout/:username', (req, res) => {
+  const { username } = req.params;
+  res.send(`<b>${username} successfully logged out.</b>`);
+});
+
+/*
+Add error handling middleware to handle below error
+- Return 500 page with message "Server Error"
+*/
+app.use((err, req, res, next) => {
+  res.status(500).send('Server Error');
+});
+
+// Use the router
+app.use('/', router);
+
+// Start the server
+app.listen(process.env.PORT || 8081, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 8081));
+});
