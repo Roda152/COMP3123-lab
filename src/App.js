72bd19a21978a20a2d814a6800a128103f2a7a23
Roda <rodaissa908@gmail.com>
2024-11-02 16:57:22 -0400

    Lab Exercise 7

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="React Logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <p>ID: 101408170</p>
+        <p>Roda Issa</p>
+        <p>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
